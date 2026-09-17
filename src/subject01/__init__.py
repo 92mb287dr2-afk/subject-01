c40dbdc31502f451133f2bf8001af45cb53565bd
92mb287dr2-afk <92mb287dr2@privaterelay.appleid.com>
2026-09-17 22:15:16 +0300

    Add src/subject01/__init__.py

---
+++ src/subject01/__init__.py
@@ -0,0 +1,7 @@
+"""Subject-01 deterministic simulation foundation."""
+
+from .core import SimulationConfig, SimulationCore
+from .runtime import SimulationRuntime
+
+__all__ = ["SimulationConfig", "SimulationCore", "SimulationRuntime"]
+__version__ = "0.1.0"
